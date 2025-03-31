c07fd3efe4a642a2daeea4db978249aff5bc47b6
viviane <vivianeaguiarc@outlook.com>
2025-03-31 17:26:14 -0300

    feat:create project, technology and database modules

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,10 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get("ping")
   getHello(): string {
-    return this.appService.getHello();
+    return "pong";
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { DbModule } from './db/db.module';
+import { ProjectModule } from './project/project.module';
+import { TechnologyModule } from './technology/technology.module';
 
 @Module({
-  imports: [],
+  imports: [DbModule, ProjectModule, TechnologyModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [],
 })
 export class AppModule {}
